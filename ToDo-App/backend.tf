b90ef05263e082101cdc93be70f86b3b43c76bfa
suvimanikandan <skmswedha.com>
2023-09-28 15:46:02 +0530

    first upload

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "10weeksofcloudops-week4"
+    bucket = "10weeksofcloudopsweek4"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
-    dynamodb_table = "10weeksofcloudops-dynamo"
+    dynamodb_table = "10weeksofcloudopsdynamo"
   }
 }
